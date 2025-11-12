96a836d3f8115c1c73123d44a80a6189a6304739
steamproof <93405617+steamproof@users.noreply.github.com>
2025-11-11 22:15:22 -0800

    lesson 7

---
+++ lesson7/classwork/practice.js
@@ -0,0 +1,22 @@
+// Problem 1
+// Find and print the total sum of all the numbers in the list.
+
+
+
+// Problem 2
+// Find and print the biggest number in the list.
+
+
+
+// Problem 3
+// Find and print the sum of only the negative numbers in the list (negative means less than 0).
+
+
+
+// Problem 4
+// Find and print the sum of only the even numbers in the list.
+
+
+
+// Problem 5
+// Find and print the biggest number that is negative in the list.
---
+++ lesson7/homework/problems.js
@@ -0,0 +1,22 @@
+// Problem 1
+// Find and print the sum of all the numbers greater than 25 in the list.
+
+
+
+// Problem 2
+// Find and print the sum of all the numbers less than -10 in the list.
+
+
+
+// Problem 3
+// Find and print the biggest number less than 100 in the list.
+
+
+
+// Problem 4
+// Find and print the biggest number in the list.
+
+
+
+// Problem 5
+// Find and print the total sum of all the numbers in the list.
