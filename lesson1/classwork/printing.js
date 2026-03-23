1433bc9baa17baf2ad753f438848e719fc390d76
SalmaamMirza <amscammingpeople@gmail.com>
2026-03-23 00:57:49 +0000

    finished lesson 1

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("hello world!");
+console.log("my name is salmaan.");
+console.log('my favorite food is better chinken.');
+
+// error: quatation marks mismatched
+// console.log('this doesnt work");
---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,8 @@
+let x = 5;  // number: whole-number value
+console.log(x);
+
+let number = 3.14;  // number: decimal value
+console.log(number);
+
+let favorite_food = "pizza";  // string: text
+console.log("my favorite food is" + favorite_food);
